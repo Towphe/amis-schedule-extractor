407ec27af884a013802a39c6287caf3e16988e53
JC Castillo <castillochristopherjames@gmail.com>
2024-08-17 01:04:30 +0800

    implemented Google Calendar integration

--- popup.js
+++ popup.js
@@ -105,6 +105,108 @@ function extractSchedule(amisDOM)
     }, 2000);
 }
 
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function formatDate(date) {
+    // return timezone compliant date
+    let formattedDate = new Date(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()+1} 01:00:00`);
+    
+    return formattedDate.toISOString().replace(/[-:]/g, '').split('T')[0];
+}
+
+function getRelevantScheduleData(enlistedSubjects)
+{
+    let jsonSchedule = [];
+
+    for (let subject of enlistedSubjects)
+    {
+        // add row per day in lecture
+        for (let lectureDay of subject.lectureDays)
+        {
+            // csvSchedule += `"${lectureDay}","${subject.lectureTimeStart}","${subject.lectureTimeEnd}","${subject.name}","${subject.lectureLocation}"\n`;
+            jsonSchedule.push({
+                day: lectureDay,
+                startTime: subject.lectureTimeStart,
+                endTime: subject.lectureTimeEnd,
+                subject: subject.name,
+                room: subject.lectureLocation
+            });
+        }
+        
+        // add row per day in lab/recit
+        if (subject.laboratorySection != null)
+           {
+            for (let labDay of subject.labDays)
+            {
+            //    csvSchedule += `"${labDay}","${subject.labTimeStart}","${subject.labTimeEnd}","${subject.name}","${subject.labLocation}"\n`;
+                jsonSchedule.push({
+                    day: labDay,
+                    startTime: subject.labTimeStart,
+                    endTime: subject.labTimeEnd,
+                    subject: `${subject.name}-${subject.laboratorySection}`,
+                    room: subject.labLocation
+                });
+            }
+        }
+    }
+
+    return jsonSchedule;
+}
+
+function getNextDayOfWeek(startDate, dayAbbr) {
+    const daysOfWeek = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+    const targetDay = daysOfWeek.indexOf(dayAbbr);
+    const startDay = startDate.getDay();
+    const daysToAdd = (targetDay + 7 - startDay) % 7;
+    const result = new Date(startDate);
+    result.setDate(result.getDate() + daysToAdd);
+    return result;
+}
+
+function getSemesterDates() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+
+    let startDate, endDate;
+
+    // this is currently set to A.Y. 2024-2025
+    if (month >= 7 && month <= 11) { // Aug - Dec
+        startDate = new Date(year, 7, 19); // August 19
+        endDate = new Date(year, 11, 12); // December 12
+    } else if (month >= 0 && month <= 5) { // Jan - Jun
+        startDate = new Date(year+1, 0, 27); // January 27
+        endDate = new Date(year, 4, 23); // May 23
+    } else { // Jun - Jul
+        startDate = new Date(year, 5, 16); // June 16
+        endDate = new Date(year, 6, 19); // July 19
+    }
+
+    return { startDate, endDate };
+}
+
+/**
+ * 
+ * @param {string} day 
+ */
+function parseDay(day)
+{
+    const dayAbbreviations = {
+        'SU': 'SU',
+        'M': 'MO',
+        'T': 'TU',
+        'W': 'WE',
+        'TH': 'TH',
+        'F': 'FR',
+        'S': 'SA',
+    };
+
+    return dayAbbreviations[day] || '';
+}
+
 const JSONToFile = (obj, filename) => {
     const blob = new Blob([JSON.stringify(obj, null, 2)], {
       type: 'application/json',
@@ -116,6 +218,50 @@ const JSONToFile = (obj, filename) => {
     a.click();
     URL.revokeObjectURL(url);
   };
+
+/**
+ * 
+ * @param {string} timeString 
+ */
+function parseTime(timeString)
+{
+    // validate time here (?) - for future implementation
+
+    let treatedTimeString = timeString;
+    treatedTimeString = treatedTimeString.replace(":", "");
+
+    const untreatedHour = parseInt(timeString.substring(0,2));
+
+    if (timeString.endsWith("PM") && untreatedHour != 12)
+    {
+        // afternoon 1PM-11:59PM
+        treatedTimeString = treatedTimeString.replace("PM", "");
+        let treatedHour = parseInt(treatedTimeString.substring(0,2)) + 12;
+        return `${treatedHour}${treatedTimeString.substring(2,4)}00`;
+    }
+    else if (timeString.endsWith("AM") && untreatedHour === 12)
+    {
+        // midnight 12AM-12:59AM
+        treatedTimeString = treatedTimeString.replace("AM", "");
+        treatedTimeString += "00";
+        return treatedTimeString.replace("12","00");   
+    }
+    else if (timeString.endsWith("PM") && untreatedHour == 12)
+    {
+        // lunch 12PM-12:59PM
+        treatedTimeString = treatedTimeString.replace("PM", "");
+        treatedTimeString += "00";
+        return treatedTimeString;   
+    }
+    else
+    {
+        // morning 1AM-11:59AM
+        treatedTimeString = treatedTimeString.replace("AM", "");
+        treatedTimeString += "00";
+        return treatedTimeString;   
+    }
+    
+}
 
 // initialize
 async function initialize()
@@ -156,6 +302,38 @@ async function initialize()
             })
         });
 
+        // convert to ICS
+        document.getElementById("gcal-dl").addEventListener("click", () => {
+            let schedule = getRelevantScheduleData(enlistedSubjects);
+
+            const { startDate, endDate } = getSemesterDates();
+            let icsContent = 'BEGIN:VCALENDAR\nVERSION:1.0\nPRODID:-//UPLB AMIS Schedule EXTRACTOR//EN\n';
+            for (let course of schedule)
+            {
+                const dayAbbr = parseDay(course.day);
+                const start = parseTime(course.startTime);
+                const end = parseTime(course.endTime);
+                const eventStart = getNextDayOfWeek(startDate, dayAbbr);
+                icsContent += 'BEGIN:VEVENT\n';
+                icsContent += `SUMMARY:${course.subject}\n`;
+                icsContent += `RRULE:FREQ=WEEKLY;BYDAY=${dayAbbr};UNTIL=${formatDate(endDate)}T235959Z\n`; // T235959Z 
+                icsContent += `DTSTART:${formatDate(eventStart)}T${start}\n`;
+                icsContent += `DTEND:${formatDate(eventStart)}T${end}\n`;
+                icsContent += 'END:VEVENT\n';
+            }
+            icsContent += 'END:VCALENDAR';
+            
+            const blob = new Blob([icsContent], {
+                type: 'text/calendar'
+            });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `1S2425-SCHED.ics`;
+            a.click();
+            URL.revokeObjectURL(url);
+        });
+
         // convert to CSV
         document.getElementById("csv-dl").addEventListener("click", () => {
             let csvSchedule = 'Day,Start Time,End Time,Class,Location\n';
@@ -178,8 +356,6 @@ async function initialize()
                 }
             }
 
-            console.log(csvSchedule);
-
             const blob = new Blob([csvSchedule], {
                 type: 'text/csv',
             });
@@ -193,40 +369,7 @@ async function initialize()
 
         // convert to JSON
         document.getElementById("json-dl").addEventListener("click", () => {
-            // console.log(JSON.stringify(enlistedSubjects));
-            let jsonSchedule = [];
-
-            for (let subject of enlistedSubjects)
-            {
-                // add row per day in lecture
-                for (let lectureDay of subject.lectureDays)
-                {
-                    // csvSchedule += `"${lectureDay}","${subject.lectureTimeStart}","${subject.lectureTimeEnd}","${subject.name}","${subject.lectureLocation}"\n`;
-                    jsonSchedule.push({
-                        day: lectureDay,
-                        startTime: subject.lectureTimeStart,
-                        endTime: subject.lectureTimeEnd,
-                        subject: subject.name,
-                        room: subject.lectureLocation
-                    });
-                }
-    
-                // add row per day in lab/recit
-                if (subject.laboratorySection != null)
-                {
-                    for (let labDay of subject.labDays)
-                    {
-                    //    csvSchedule += `"${labDay}","${subject.labTimeStart}","${subject.labTimeEnd}","${subject.name}","${subject.labLocation}"\n`;
-                        jsonSchedule.push({
-                            day: labDay,
-                            startTime: subject.labTimeStart,
-                            endTime: subject.labTimeEnd,
-                            subject: `${subject.name}-${subject.laboratorySection}`,
-                            room: subject.labLocation
-                        });
-                    }
-                }
-            }
+            let jsonSchedule = getRelevantScheduleData(enlistedSubjects);
 
             JSONToFile(jsonSchedule, "1S2425-SCHED");
         });
